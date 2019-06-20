60acda284d4b98eae9cecd62ca24f3d618354124
Harsh Vardhan Goswami <46974223+theuitown@users.noreply.github.com>
2019-06-20 12:17:35 +0530

    Add files via upload

---
+++ portfolio 1.0 sppedcode/js/style.js
@@ -0,0 +1,24 @@
+const harsh = baffle(".what_do");
+harsh.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+harsh.start();
+harsh.reveal(4000);
+
+const harshman = baffle(".headingtext");
+harshman.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+harshman.start();
+harshman.reveal(4000);
+
+const iamharsh = baffle(".heading_subtext");                                                                                                                    
+iamharsh.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+iamharsh.start();
+iamharsh.reveal(4000);
+
